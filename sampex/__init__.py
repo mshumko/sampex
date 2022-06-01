874d339d70208cc2ff2ef5290472f2f609c5b5c4
Mykhaylo Shumko <msshumko@gmail.com>
2022-06-01 06:45:02 -0400

    Turned it into a package.

---
+++ sampex/__init__.py
@@ -0,0 +1,21 @@
+import warnings
+import pathlib
+import configparser
+
+__version__ = '0.0.1'
+
+# Load the configuration settings.
+here = pathlib.Path(__file__).parent.resolve()
+settings = configparser.ConfigParser()
+settings.read(here / 'config.ini')
+
+# Go here if config.ini exists (don't crash if the project is not yet configured.)
+if 'Paths' in settings:  
+    try:
+        sampex_code_dir = settings['Paths']['sampex_code_dir']
+        sampex_data_dir = settings['Paths']['sampex_data_dir']
+    except KeyError as err:
+        warnings.warn('The sampex package did not find the config.ini file. '
+            'Did you run "python3 -m sampex config"?')
+
+    config = {'sampex_code_dir': sampex_code_dir, 'sampex_data_dir': sampex_data_dir}
---
+++ sampex/__main__.py
@@ -0,0 +1,52 @@
+import sys
+import pathlib
+import configparser
+
+import sampex
+
+# Run the configuration script with
+# python3 -m sampex [init, initialize, config, or configure]
+
+here = pathlib.Path(__file__).parent.resolve()
+
+if (len(sys.argv) > 1) and (sys.argv[1] in ['init', 'initialize', 'config', 'configure']):
+    print('Running the configuration script.')
+    s = (
+        f"What is your sampex data directory? Press enter for the default "
+        f"directory at ~/sampex/ folder will be created if it doesn't exist.\n"
+    )
+    sampex_data_dir = input(s)
+
+    # If the user specified the directory, check that the directory already exists
+    # and make that directory if it does not.
+    if sampex_data_dir != '':
+        if not pathlib.Path(sampex_data_dir).exists():
+            pathlib.Path(sampex_data_dir).mkdir(parents=True)
+            print(f'Made sampex data directory at {pathlib.Path(sampex_data_dir)}.')
+        else:
+            print(f'The sampex data directory at {pathlib.Path(sampex_data_dir)} already exists.')
+    else:
+        # If the user did not specify the directory, make one at ~/sampex/.
+        sampex_data_dir = pathlib.Path.home() / 'sampex'
+        if not sampex_data_dir.exists():
+            sampex_data_dir.mkdir(parents=True)
+            print(f'sampex directory at {sampex_data_dir} created.')
+        else:
+            print(f'sampex directory at {sampex_data_dir} already exists.')
+
+    # Create a configparser object and add the user configuration.
+    config = configparser.ConfigParser()
+    config['Paths'] = {
+        'sampex_code_dir':here,
+        'sampex_data_dir': sampex_data_dir
+        }
+
+    with open(here / 'config.ini', 'w') as f:
+        config.write(f)
+
+else:
+    print(
+        'This is a configuration script to set up config.ini file. The config '
+        'file contains the top-level sampex data directory, ~/sampex/ by '
+        'default. To configure this package, run python3 -m sampex config.'
+    )
