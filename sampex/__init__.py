7351174ac417dc4481ec5f30148fdeb89fe889b6
Mykhaylo Shumko <msshumko@gmail.com>
2022-07-09 18:08:50 -0400

    Bump version: 0.2.0 → 0.2.1

--- sampex/__init__.py
+++ sampex/__init__.py
@@ -2,7 +2,7 @@ import warnings
 import pathlib
 import configparser
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 # Load the configuration settings.
 here = pathlib.Path(__file__).parent.resolve()
